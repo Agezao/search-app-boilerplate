936edabbe41c9eb012a53ee3784c25f9cf149c1d
Age <0800.grc@gmail.com>
2020-06-30 00:16:43 -0300

    Adding search filters to athlete business

--- api/business/athlete.business.js
+++ api/business/athlete.business.js
@@ -1,12 +1,42 @@
+const {gte, or, lte, contains, in: opIn, like} = require('sequelize').Op;
 const models = require('../models');
 
-const get = async () => {
+const get = async ({
+  name,
+  ageRangeStart,
+  ageRangeEnd,
+  skills,
+  professionalExperienceYears,
+} = {}) => {
+
+  const where = {
+    birthDate: {},
+  };
+
+  if (name) where.name = { [like]: `%${name}%` };
+
+  if (ageRangeStart) where.birthDate = { ...where.birthDate, [lte]: ageRangeStart };
+
+  if (ageRangeEnd) where.birthDate = { ...where.birthDate, [gte]: ageRangeEnd };
+
+  if (skills) {
+    where[or] = [
+      { "$skills.name$": { [opIn]: skills } },
+      { "$skills.superset$": { [opIn]: skills } }
+    ];
+  }
+
+  if (professionalExperienceYears) where.firstProfessionalExperienceDate = { [lte]: professionalExperienceYears };
+
+  const include = [
+    { model: models.Skill, as: 'skills' },
+    { model: models.AthleteChampionship, as: 'championships' },
+  ];
+
   return models.Athlete.findAll({
-      include: [
-        { model: models.Skill, as: 'skills' },
-        { model: models.AthleteChampionship, as: 'championships' },
-      ]
-    });
+    include,
+    where,
+  });
 }
 
 module.exports = {
