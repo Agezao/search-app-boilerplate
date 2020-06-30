1fed559c5ac29df7b49759b56b83e232d7d107ac
Age <0800.grc@gmail.com>
2020-06-30 00:17:12 -0300

    Adding athlete errors to errors enum

--- api/enums/error-codes.enum.js
+++ api/enums/error-codes.enum.js
@@ -2,6 +2,9 @@ const errorCodes = {
   'UNEXPECTED': 100,
   'VALIDATION_ERROR': 101,
   'NOT_FOUND': 103,
+  'ATHLETES': {
+    'INVALID_SEARCH_PARAMS': 1000,
+  }
 };
 
 module.exports = { ...errorCodes };
